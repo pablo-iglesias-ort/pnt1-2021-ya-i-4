d8b956b38c8c95e4af7ae225ec3fc019a9fb4140
Franco Redoni <A309742@santandertecnologia.com.ar>
2021-09-07 11:14:58 -0300

    Agregado de entidades Empleado, Teléfono y Usuario

---
+++ ERPBasico/ERPBasico/Models/PersonaBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPBasico.Models
+{
+    public class PersonaBase : EntidadBase
+    {
+        [Required(ErrorMessage = "Nombre es un campo requerido"), StringLength(30, ErrorMessage = "Nombre demasiado largo")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Email es un campo requerido")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
