16f90dacf3ff577e6efd9041dccc55d62e09d9bf
BRUNO DANIEL BONINO <38658340@comunidadort.edu.ar>
2021-09-07 11:44:55 -0300

    Creo los modelos TipoTelefono ,Posicion ,Imagen

---
+++ ERPBasico/ERPBasico/Models/ImagenModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ERPBasico.Models
+{
+    public class ImagenModel
+    {
+        public String nombreImagen { get; set; };
+        public byte[] imagen { get; set; };
+    }
+}
---
+++ ERPBasico/ERPBasico/Models/TipoTelefonoModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ERPBasico.Models
+{
+    public enum TipoTelefonoModel
+    {
+        TELEFONO_FIJO,TELEFONO_CELULAR;
+    }
+}
+
