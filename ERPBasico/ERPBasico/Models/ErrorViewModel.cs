ced072d9dc9f9413b70552b88569b6364cf87f4c
Franco Redoni <A309742@santandertecnologia.com.ar>
2021-09-08 15:39:42 -0300

    gh

--- ERPBasico/ERPBasico/Models/ErrorViewModel.cs
+++ ERPBasico/ERPBasico/Models/ErrorViewModel.cs
@@ -5,6 +5,6 @@ namespace ERPBasico.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; };
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId)
     }
 }
