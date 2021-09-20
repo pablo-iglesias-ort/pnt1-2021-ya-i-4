2dfdb6ebb4b70a22ce6bb257892f9f973687fda2
BRUNO DANIEL BONINO <38658340@comunidadort.edu.ar>
2021-09-20 15:26:25 -0300

    Se agregan validaciones

--- ERPBasico/ERPBasico/Models/Empresa.cs
+++ ERPBasico/ERPBasico/Models/Empresa.cs
@@ -4,11 +4,15 @@ namespace ERPBasico.Models
 {
     public class Empresa : EntidadBase
     {
+        [Required(ErrorMessage = "Nombre es un campo requerido")]
         public string Nombre { get; set; }
         public Rubros rubro { get; set; }
         public Imagen Logo { get; set; }
+        [range(4,100, ErrorManssage="Mail Invalido"),Required(ErrorMenssage= "La direccion es un campo requerido")]
         public string Direccion { get; set; }
+        [Required(ErrorMenssage = "El telefono de contacto es un campo requerido")]
         public Telefono TelefonoContacto { get; set; }
+        [Required(ErrorMenssage = "El mail de contacto es un campo requerido")]
         public string EmailContacto { get; set; }
 
     }
--- ERPBasico/ERPBasico/Models/Gerencia.cs
+++ ERPBasico/ERPBasico/Models/Gerencia.cs
@@ -4,8 +4,10 @@ namespace ERPBasico.Models
 {
     public class Gerencia : EntidadBase
     {
+        [Required(ErrorMessage = "Nombre es un campo requerido")]
         public String Nombre { get; set; }
         public bool EsGerenciaGeneral { get; set; }
+        [Required, StringLength(20, ErrorMessage = "Dirección es un campo requerido")]
         public Gerencia Direccion { get; set; }
         public Posicion Responsable { get; set; }
         public Posicion[] Posiciones{ get; set; }
